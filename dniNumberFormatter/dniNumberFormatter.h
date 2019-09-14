dbb972a354c3d26bae23cc0db65248c2803fc7bb
Jeff Hitchcock <mail@command-center.org>
2019-09-14 10:38:43 -0700

    Initial Commit

---
+++ dniNumberFormatter/dniNumberFormatter.h
@@ -0,0 +1,19 @@
+//
+//  dniNumberFormatter.h
+//  dniNumberFormatter
+//
+//  Created by Jeff Hitchcock on 2019-09-14.
+//  Copyright © 2019 Jeff Hitchcock. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for dniNumberFormatter.
+FOUNDATION_EXPORT double dniNumberFormatterVersionNumber;
+
+//! Project version string for dniNumberFormatter.
+FOUNDATION_EXPORT const unsigned char dniNumberFormatterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <dniNumberFormatter/PublicHeader.h>
+
+
